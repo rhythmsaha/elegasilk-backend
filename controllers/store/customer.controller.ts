f99efe24253794da514a1a071453c11445f15d7c
Rhythm Saha <rs.2001.saha@gmail.com>
2024-03-15 00:05:46 +0530

    Add customer-related routes

--- controllers/store/customer.controller.ts
+++ controllers/store/customer.controller.ts
@@ -7,6 +7,7 @@ import VerificationToken, { IVerificationToken } from "../../models/store/Verifi
 import CustomerSession from "../../utils/customer/CustomerSession";
 import generateOTP from "../../utils/generateOtp";
 import VerificationCode from "../../models/verificationcode.model";
+import crypto from "crypto";
 
 const checkRequestPermission = (id: string, req: Request, res: Response, next: NextFunction) => {
     if (id !== req.customer?._id) {
@@ -23,58 +24,54 @@ const checkRequestPermission = (id: string, req: Request, res: Response, next: N
  * @returns A JSON response indicating the success or failure of the customer creation.
  * @throws {ErrorHandler} If there is an error during customer creation.
  */
-export const createCustomer = asyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-        const { firstName, lastName, email, password } = req.body as ICustomer & {
-            password: string;
-        };
+export const createCustomer = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { firstName, lastName, email, password } = req.body as ICustomer & {
+        password: string;
+    };
 
-        try {
-            // Check if admin already exists
-            const customerExists = await Customer.findOne({ email });
+    try {
+        // Check if admin already exists
+        const customerExists = await Customer.findOne({ email });
 
-            if (customerExists && customerExists.verified)
-                return next(new ErrorHandler("Customer already exists", 400));
+        if (customerExists && customerExists.verified) return next(new ErrorHandler("Customer already exists", 400));
 
-            if (customerExists && !customerExists.verified)
-                return resendVerificationLink(req, res, next);
+        if (customerExists && !customerExists.verified) return resendVerificationLink(req, res, next);
 
-            // Create new customer
-            const newCustomer = await Customer.create({
-                firstName,
-                lastName,
-                email,
-                hashed_password: password,
-            });
+        // Create new customer
+        const newCustomer = await Customer.create({
+            firstName,
+            lastName,
+            email,
+            hashed_password: password,
+        });
 
-            if (!newCustomer) return next(new ErrorHandler("Customer registration failed", 400));
+        if (!newCustomer) return next(new ErrorHandler("Customer registration failed", 400));
 
-            // Generate verification token
-            const token = new VerificationToken().createVerificationToken(newCustomer._id);
-            const verificationToken = await VerificationToken.create({
-                userId: newCustomer._id,
-                expireAt: new Date(Date.now() + 3600000),
-                token: token,
-            });
+        // Generate verification token
+        const token = new VerificationToken().createVerificationToken(newCustomer._id);
+        const verificationToken = await VerificationToken.create({
+            userId: newCustomer._id,
+            expireAt: new Date(Date.now() + 3600000),
+            token: token,
+        });
 
-            if (!verificationToken) {
-                return next(new ErrorHandler("Customer registration failed", 400));
-            }
-
-            // Send verification email
-            const verificationLink = `http://localhost:3000/verifyaccount?token=${verificationToken.token}&customerId=${newCustomer._id}&tokenID=${verificationToken._id}`;
-
-            console.log(verificationLink);
-
-            res.status(201).json({
-                success: true,
-                message: "Please Check Your Email to Verify Your Account!",
-            });
-        } catch (error: any) {
-            return next(new ErrorHandler("Customer registration failed", error.statusCode || 500));
+        if (!verificationToken) {
+            return next(new ErrorHandler("Customer registration failed", 400));
         }
+
+        // Send verification email
+        const verificationLink = `http://localhost:3000/verifyaccount?token=${verificationToken.token}&customerId=${newCustomer._id}&tokenID=${verificationToken._id}`;
+
+        console.log(verificationLink);
+
+        res.status(201).json({
+            success: true,
+            message: "Please Check Your Email to Verify Your Account!",
+        });
+    } catch (error: any) {
+        return next(new ErrorHandler("Customer registration failed", error.statusCode || 500));
     }
-);
+});
 
 /**
  * Resends the verification link to the customer's email.
@@ -84,51 +81,49 @@ export const createCustomer = asyncHandler(
  * @param next - The next function.
  * @returns A JSON response indicating the success status, verification link, and message.
  */
-export const resendVerificationLink = asyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-        const { email } = req.body as ICustomer;
+export const resendVerificationLink = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { email } = req.body as ICustomer;
 
-        if (!validator.isEmail(email)) {
-            return next(new ErrorHandler("Invalid email", 400));
-        }
-
-        const customer = await Customer.findOne({ email });
-
-        if (!customer) {
-            return next(new ErrorHandler("Account not found", 400));
-        }
-
-        if (customer.verified) {
-            return next(new ErrorHandler("Account already verified", 400));
-        }
-
-        await VerificationToken.findOneAndDelete({ userId: customer._id });
-
-        const token = new VerificationToken().createVerificationToken(customer._id);
-        const _verificationToken = new VerificationToken({
-            userId: customer._id,
-            expireAt: new Date(Date.now() + 3600000),
-            token: token,
-        });
-
-        const saveToken = await _verificationToken.save();
-
-        if (!saveToken) {
-            return next(new ErrorHandler("Failed to resend verification link", 400));
-        }
-
-        // Send verification email
-        const verificationLink = `http://localhost:3000/verifyaccount?token=${token}&customerId=${customer._id}&tokenID=${saveToken._id}`;
-
-        console.log(verificationLink);
-
-        res.status(201).json({
-            success: true,
-            link: verificationLink,
-            message: "Verification link sent successfully",
-        });
+    if (!validator.isEmail(email)) {
+        return next(new ErrorHandler("Invalid email", 400));
     }
-);
+
+    const customer = await Customer.findOne({ email });
+
+    if (!customer) {
+        return next(new ErrorHandler("Account not found", 400));
+    }
+
+    if (customer.verified) {
+        return next(new ErrorHandler("Account already verified", 400));
+    }
+
+    await VerificationToken.findOneAndDelete({ userId: customer._id });
+
+    const token = new VerificationToken().createVerificationToken(customer._id);
+    const _verificationToken = new VerificationToken({
+        userId: customer._id,
+        expireAt: new Date(Date.now() + 3600000),
+        token: token,
+    });
+
+    const saveToken = await _verificationToken.save();
+
+    if (!saveToken) {
+        return next(new ErrorHandler("Failed to resend verification link", 400));
+    }
+
+    // Send verification email
+    const verificationLink = `http://localhost:3000/verifyaccount?token=${token}&customerId=${customer._id}&tokenID=${saveToken._id}`;
+
+    console.log(verificationLink);
+
+    res.status(201).json({
+        success: true,
+        link: verificationLink,
+        message: "Verification link sent successfully",
+    });
+});
 
 /**
  * Verifies a customer account using a token and userId.
@@ -137,58 +132,56 @@ export const resendVerificationLink = asyncHandler(
  * @param next - The next function.
  * @returns A JSON response indicating the success or failure of the account verification.
  */
-export const verifyCustomerAccount = asyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-        const { token, userId, tokenId } = req.query as {
-            token: string;
-            userId: string;
-            tokenId: string;
-        };
+export const verifyCustomerAccount = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { token, userId, tokenId } = req.query as {
+        token: string;
+        userId: string;
+        tokenId: string;
+    };
 
-        if (!userId || !token || !tokenId) {
+    if (!userId || !token || !tokenId) {
+        return next(new ErrorHandler("Invalid token", 400));
+    }
+
+    try {
+        const verificationToken = await VerificationToken.findById(tokenId);
+
+        if (!verificationToken) {
             return next(new ErrorHandler("Invalid token", 400));
         }
 
-        try {
-            const verificationToken = await VerificationToken.findById(tokenId);
-
-            if (!verificationToken) {
-                return next(new ErrorHandler("Invalid token", 400));
-            }
-
-            if (verificationToken.userId.toString() !== userId) {
-                return next(new ErrorHandler("Invalid token", 400));
-            }
-
-            if (!verificationToken.verifyToken(token)) {
-                return next(new ErrorHandler("Invalid token", 400));
-            }
-
-            const customer = await Customer.findById(userId);
-
-            if (!customer) {
-                return next(new ErrorHandler("Account not found", 400));
-            }
-
-            customer.verified = true;
-
-            const saveCustomer = await customer.save();
-
-            if (!saveCustomer) {
-                return next(new ErrorHandler("Account verification failed", 400));
-            }
-
-            await verificationToken.deleteOne();
-
-            res.status(200).json({
-                success: true,
-                message: "Account verified successfully",
-            });
-        } catch (error: any) {
-            return next(new ErrorHandler(error.message, 400));
+        if (verificationToken.userId.toString() !== userId) {
+            return next(new ErrorHandler("Invalid token", 400));
         }
+
+        if (!verificationToken.verifyToken(token)) {
+            return next(new ErrorHandler("Invalid token", 400));
+        }
+
+        const customer = await Customer.findById(userId);
+
+        if (!customer) {
+            return next(new ErrorHandler("Account not found", 400));
+        }
+
+        customer.verified = true;
+
+        const saveCustomer = await customer.save();
+
+        if (!saveCustomer) {
+            return next(new ErrorHandler("Account verification failed", 400));
+        }
+
+        await verificationToken.deleteOne();
+
+        res.status(200).json({
+            success: true,
+            message: "Account verified successfully",
+        });
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
     }
-);
+});
 
 /**
  * Logs in a customer.
@@ -198,38 +191,36 @@ export const verifyCustomerAccount = asyncHandler(
  * @param next - The next function.
  * @returns The login session of the customer.
  */
-export const loginCustomer = asyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-        const { email, password } = req.body as ICustomer & { password: string };
+export const loginCustomer = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body as ICustomer & { password: string };
 
-        if (!email || !password) {
-            return next(new ErrorHandler("Please provide email and password", 400));
-        }
-
-        const customer = await Customer.findOne({ email }).select("+hashed_password");
-
-        if (!customer) {
-            return next(new ErrorHandler("Account doesn't exist!", 404));
-        }
-
-        const comparePassword = await customer.comparePassword(password);
-
-        if (!comparePassword) {
-            return next(new ErrorHandler("Invalid email or password!", 400));
-        }
-
-        if (!customer.verified) {
-            return next(new ErrorHandler("Please verify your account!", 400));
-        }
-
-        if (!customer.status) {
-            return next(new ErrorHandler("Account is disabled!", 400));
-        }
-
-        const session = CustomerSession.from(customer).loginSession;
-        res.status(200).json(session);
+    if (!email || !password) {
+        return next(new ErrorHandler("Please provide email and password", 400));
     }
-);
+
+    const customer = await Customer.findOne({ email }).select("+hashed_password");
+
+    if (!customer) {
+        return next(new ErrorHandler("Account doesn't exist!", 404));
+    }
+
+    const comparePassword = await customer.comparePassword(password);
+
+    if (!comparePassword) {
+        return next(new ErrorHandler("Invalid email or password!", 400));
+    }
+
+    if (!customer.verified) {
+        return next(new ErrorHandler("Please verify your account!", 400));
+    }
+
+    if (!customer.status) {
+        return next(new ErrorHandler("Account is disabled!", 400));
+    }
+
+    const session = CustomerSession.from(customer).loginSession;
+    res.status(200).json(session);
+});
 
 /**
  * Refreshes the customer session.
@@ -239,26 +230,24 @@ export const loginCustomer = asyncHandler(
  * @param next - The next function.
  * @returns The refreshed customer session.
  */
-export const refreshCustomerSession = asyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-        // Get customer id from request
-        const customerId = req.customer?._id;
+export const refreshCustomerSession = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    // Get customer id from request
+    const customerId = req.customer?._id;
 
-        if (!customerId) {
-            return next(new ErrorHandler("Please login to access this resource", 401));
-        }
-        // Find customer by id
-        const customer = await Customer.findById(customerId);
-
-        if (!customer) {
-            return next(new ErrorHandler("Customer not found", 404));
-        }
-
-        // Generate new session
-        const session = CustomerSession.from(customer).refreshTokenSession;
-        res.status(200).json(session);
+    if (!customerId) {
+        return next(new ErrorHandler("Please login to access this resource", 401));
     }
-);
+    // Find customer by id
+    const customer = await Customer.findById(customerId);
+
+    if (!customer) {
+        return next(new ErrorHandler("Customer not found", 404));
+    }
+
+    // Generate new session
+    const session = CustomerSession.from(customer).refreshTokenSession;
+    res.status(200).json(session);
+});
 
 /**
  * Updates the profile of a customer.
@@ -470,3 +459,38 @@ export const verifyCustomerEmail = asyncHandler(async (req, res, next) => {
         message: "Email verified successfully",
     });
 });
+
+export const forgetPassword = asyncHandler(async (req, res, next) => {
+    const { email } = req.body as ICustomer;
+
+    if (!validator.isEmail(email)) {
+        return next(new ErrorHandler("Invalid email", 400));
+    }
+
+    const customer = await Customer.findOne({ email });
+
+    if (!customer) {
+        return next(new ErrorHandler("Account not found", 400));
+    }
+
+    const token = crypto.randomBytes(16).toString("hex");
+
+    const verificationCode = await VerificationCode.create({
+        code: token,
+    });
+
+    if (!verificationCode) {
+        return next(new ErrorHandler("Something went wrong!", 400));
+    }
+
+    const resetLink = `http://localhost:3000/resetpassword?customerId=${customer._id}&id=${verificationCode._id}&token=${verificationCode.code}`;
+
+    console.log(resetLink);
+
+    res.status(200).json({
+        success: true,
+        message: "Check your inbox to reset your password",
+    });
+});
+
+export const resetPassword = asyncHandler(async (req, res, next) => {});
--- routes/store/customer.routes.ts
+++ routes/store/customer.routes.ts
@@ -1,3 +1,6 @@
+/**
+ * Express router for handling customer-related routes.
+ */
 import express from "express";
 import { Secret } from "jsonwebtoken";
 import { authorizeAccessToken } from "../../middlewares/auth";
@@ -19,38 +22,88 @@ const CustomerRouter = express.Router();
 
 const CUSTOMER_JWT_SECRET = process.env.CUSTOMER_JWT_SECRET as Secret;
 
+/**
+ * Route for registering a new customer.
+ * @name POST /register
+ * @function
+ */
 CustomerRouter.post("/register", createCustomer);
 
+/**
+ * Route for resending the verification link to a customer's email.
+ * @name POST /resendverificationlink
+ * @function
+ */
 CustomerRouter.post("/resendverificationlink", resendVerificationLink);
 
+/**
+ * Route for verifying a customer's account.
+ * @name GET /verifyaccount
+ * @function
+ */
 CustomerRouter.get("/verifyaccount", verifyCustomerAccount);
 
+/**
+ * Route for logging in a customer.
+ * @name POST /login
+ * @function
+ */
 CustomerRouter.post("/login", loginCustomer);
 
-CustomerRouter.get(
-    "/refresh-session",
-    authorizeAccessToken(CUSTOMER_JWT_SECRET, true),
-    refreshCustomerSession
-);
+/**
+ * Route for refreshing a customer's session.
+ * @name GET /refresh-session
+ * @function
+ * @middleware authorizeAccessToken
+ */
+CustomerRouter.get("/refresh-session", authorizeAccessToken(CUSTOMER_JWT_SECRET, true), refreshCustomerSession);
 
+/**
+ * Route for updating a customer's email.
+ * @name POST /email/:id
+ * @function
+ * @middleware authorizeAccessToken
+ */
 CustomerRouter.post("/email/:id", authorizeAccessToken(CUSTOMER_JWT_SECRET), updateCustomerEmail);
 
-CustomerRouter.put(
-    "/email/verify/:id",
-    authorizeAccessToken(CUSTOMER_JWT_SECRET),
-    verifyCustomerEmail
-);
+/**
+ * Route for verifying a customer's email.
+ * @name PUT /email/verify/:id
+ * @function
+ * @middleware authorizeAccessToken
+ */
+CustomerRouter.put("/email/verify/:id", authorizeAccessToken(CUSTOMER_JWT_SECRET), verifyCustomerEmail);
 
-CustomerRouter.put(
-    "/password/:id",
-    authorizeAccessToken(CUSTOMER_JWT_SECRET),
-    updateCustomerPassword
-);
+/**
+ * Route for updating a customer's password.
+ * @name PUT /password/:id
+ * @function
+ * @middleware authorizeAccessToken
+ */
+CustomerRouter.put("/password/:id", authorizeAccessToken(CUSTOMER_JWT_SECRET), updateCustomerPassword);
 
+/**
+ * Route for retrieving a customer's profile.
+ * @name GET /:id
+ * @function
+ * @middleware authorizeAccessToken
+ */
 CustomerRouter.get("/:id", authorizeAccessToken(CUSTOMER_JWT_SECRET), getCustomerProfile);
 
+/**
+ * Route for updating a customer's profile.
+ * @name PUT /:id
+ * @function
+ * @middleware authorizeAccessToken
+ */
 CustomerRouter.put("/:id", authorizeAccessToken(CUSTOMER_JWT_SECRET), updateCustomerProfile);
 
+/**
+ * Route for deleting a customer's account.
+ * @name DELETE /:id
+ * @function
+ * @middleware authorizeAccessToken
+ */
 CustomerRouter.delete("/:id", authorizeAccessToken(CUSTOMER_JWT_SECRET), deleteCustomerAccount);
 
 export default CustomerRouter;
