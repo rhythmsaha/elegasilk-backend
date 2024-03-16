0d74823ca3de657f3a866c7449254d36e73608dd
Rhythm Saha <rs.2001.saha@gmail.com>
2024-03-16 16:03:19 +0530

    Add wishlist routes and authorization

--- routes/store/wishlist.routes.ts
+++ routes/store/wishlist.routes.ts
@@ -9,12 +9,41 @@ import {
     removeItemFromWishlist,
 } from "../../controllers/store/wishlist.controller";
 
+/**
+ * Express router for managing wishlist routes.
+ */
 const wishlistRouter = express.Router();
 
+/**
+ * Route for adding an item to the wishlist.
+ * Requires authorization with customer JWT secret.
+ */
 wishlistRouter.post("/add", authorizeAccessToken(CUSTOMER_JWT_SECRET, true), addItemToWishlist);
+
+/**
+ * Route for removing an item from the wishlist.
+ * Requires authorization with customer JWT secret.
+ * @param productId - The ID of the product to be removed from the wishlist.
+ */
 wishlistRouter.delete("/remove/:productId", authorizeAccessToken(CUSTOMER_JWT_SECRET, true), removeItemFromWishlist);
+
+/**
+ * Route for checking if a product is in the wishlist.
+ * Requires authorization with customer JWT secret.
+ * @param productId - The ID of the product to check in the wishlist.
+ */
 wishlistRouter.get("/:productId", authorizeAccessToken(CUSTOMER_JWT_SECRET, true), checkProductInWishlist);
+
+/**
+ * Route for getting the wishlist.
+ * Requires authorization with customer JWT secret.
+ */
 wishlistRouter.get("/", authorizeAccessToken(CUSTOMER_JWT_SECRET, true), getWishlist);
+
+/**
+ * Route for clearing the wishlist.
+ * Requires authorization with customer JWT secret.
+ */
 wishlistRouter.delete("/", authorizeAccessToken(CUSTOMER_JWT_SECRET, true), clearWishlist);
 
 export default wishlistRouter;
