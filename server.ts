59839479472139385a13ab4626a87fcc5d566ff6
Rhythm Saha <rs.2001.saha@gmail.com>
2024-03-30 18:47:34 +0530

    Refactor server.ts to remove cluster module and simplify server creation

--- server.ts
+++ server.ts
@@ -1,12 +1,8 @@
-import cluster from "node:cluster";
-import os from "node:os";
 import process from "node:process";
 
 import dotenv from "dotenv";
 import { app } from "./app";
 import connectDB from "./lib/db";
-
-const totalCpus = os.cpus().length;
 
 //For env File
 dotenv.config();
@@ -14,25 +10,16 @@ dotenv.config();
 // Create Server
 const port = process.env.PORT || 8000;
 
-if (cluster.isPrimary) {
-    console.log(`Primary ${process.pid} is running`);
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${port}`);
+    connectDB();
+});
 
-    for (let i = 0; i < totalCpus; i++) {
-        cluster.fork();
-    }
-} else {
-    const server = app.listen(process.env.PORT, () => {
-        console.log(`total cpus: ${totalCpus}`);
-        console.log(`Server listening on port ${port}`);
-        connectDB();
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err: any) => {
+    console.log(`Shutting down the server for ERROR: ${err.message}`);
+    console.log(`Shutting down the server for unhandled promise rejections`);
+    server.close(() => {
+        process.exit(1);
     });
-
-    // Handle unhandled promise rejections
-    process.on("unhandledRejection", (err: any) => {
-        console.log(`Shutting down the server for ERROR: ${err.message}`);
-        console.log(`Shutting down the server for unhandled promise rejections`);
-        server.close(() => {
-            process.exit(1);
-        });
-    });
-}
+});
